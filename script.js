e594778fdc390eed7e5a17a4475759e8bb379766
Frederico Sá de Andrade <13704867667@CAMPUS.MACHADO.MCH>
2023-05-03 22:04:59 -0300

    second commit

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-function converter(){
+function converter() {
     const valor = document.getElementById("valor").value;
     const base_origem = document.getElementById("base_origem").value;
     const base_destino = document.getElementById("base_destino").value;
-    
+
     let resultado;
 
-    if (base_origem === "binario"){
+    if (base_origem === "binario") {
         resultado = converter_binario(valor, base_destino);
     }
-    else if (base_origem === "hexadecimal"){
+    else if (base_origem === "hexadecimal") {
         resultado = converter_hexadecimal(valor, base_destino);
-    }   
-    else if (base_origem === "octal"){
+    }
+    else if (base_origem === "octal") {
         resultado = converter_octal(valor, base_destino);
     }
-    else if (base_origem === "decimal"){
+    else if (base_origem === "decimal") {
         resultado = converter_decimal(valor, base_destino);
     }
 
     document.getElementById("resultado").value = resultado;
 }
 
-function converter_binario(valor, tipo){
+function converter_binario(valor, tipo) {
     switch (tipo) {
         case "hexadecimal":
             return parseInt(valor, 2).toString(16);
@@ -31,11 +31,10 @@ function converter_binario(valor, tipo){
             return parseInt(valor, 2).toString(10);
         default:
             return valor;
-            break;
     }
 }
 
-function converter_hexadecimal(valor, tipo){
+function converter_hexadecimal(valor, tipo) {
     switch (tipo) {
         case "binario":
             return parseInt(valor, 16).toString(2);
@@ -45,11 +44,10 @@ function converter_hexadecimal(valor, tipo){
             return parseInt(valor, 16).toString(10);
         default:
             return valor;
-            break;
     }
 }
 
-function converter_octal(valor, tipo){
+function converter_octal(valor, tipo) {
     switch (tipo) {
         case "binario":
             return parseInt(valor, 8).toString(2);
@@ -59,11 +57,10 @@ function converter_octal(valor, tipo){
             return parseInt(valor, 8).toString(10);
         default:
             return valor;
-            break;
     }
 }
 
-function converter_decimal(valor, tipo){
+function converter_decimal(valor, tipo) {
     switch (tipo) {
         case "binario":
             return parseInt(valor, 10).toString(2);
@@ -73,6 +70,29 @@ function converter_decimal(valor, tipo){
             return parseInt(valor, 10).toString(16);
         default:
             return valor;
-            break;
     }
 }
+
+var modal = document.getElementById("modal");
+
+function abrir_modal(){
+    modal.style.display = "block";
+}
+function fechar_modal(){
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
